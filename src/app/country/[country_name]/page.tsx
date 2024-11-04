160c29fe0b83cd726066195e6ae7399ed983d7a5
adnan29030 <smg29030@gmail.com>
2024-11-05 00:42:03 +0500

    first commit

---
+++ src/app/country/[country_name]/page.tsx
@@ -0,0 +1,49 @@
+export default function Country({params}:any) {
+  const countries : {
+        name : string,
+        population : number,
+        capital : string,
+  } [] = [{
+    name : "Pakistan",
+    population : 251269164,
+    capital : "Islamabad",
+  },{
+    name : "India",  
+    population :1441719852,
+    capital : "NewDelhi",
+  },
+  {
+    name : "SriLanka",
+    population :23103565 ,
+    capital : "Colombo",
+  },
+  {
+    name : "Bangladash",
+    population :173562364,
+    capital : "Dhaka",
+  }
+
+ ]
+  function countryname(select_url :string){
+      return countries.find(country=>country.name.toLowerCase()==select_url.toLowerCase());    
+  }
+  let result = countryname(params.country_name)
+  return (
+    <div className="bg-red-600 h-screen">
+      <h1 className="flex justify-center items-center h-96 font-extrabold text-5xl">Select Your Country</h1>
+      {
+        result ? (
+          <>
+          <h1 className="flex justify-center items-center font-bold text-3xl">Country Name : {result.name}</h1>
+          <h1 className="flex justify-center items-center font-bold text-3xl">Country Capital : {result.capital}</h1>
+          <h1 className="flex justify-center items-center font-bold text-3xl">Country population : {result.population}</h1>
+          </>
+        ):(
+          <h1 className="flex justify-center items-center font-bold text-3xl">Country Not Found</h1>
+        )
+      }
+    </div>
+            
+    );
+  }
+  
