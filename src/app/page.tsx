e5e1aab23c7855b20e87212a85b0d8042cf860a1
adnan29030 <smg29030@gmail.com>
2024-11-05 08:39:15 +0500

    change2

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,16 @@
+import Link from "next/link";
 export default function HomePage() {
     return (
-      <div>
-        <h1>Welcome to the Home Page</h1>
+        <>
+      <div className="flex flex-col text-3xl font-bold justify-center h-screen bg-orange-500 items-center ">
+        <h1 className="flex justify-center text-5xl font-extrabold mb-8">Select Your Country </h1>
+        <Link href="/country/pakistan">Pakistan</Link>
+        <Link href="/country/india">India</Link>
+        <Link href="/country/srilanka">SriLanka</Link>
+        <Link href="/country/bangladash">Bangladash</Link>
+        <Link href="/country/japan">Japan</Link>
+
       </div>
+      </>
     );
   }
